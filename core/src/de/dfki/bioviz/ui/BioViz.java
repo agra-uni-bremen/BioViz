522f9952a2b5be0548dad8b05bdf935f2a98fa86
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2015-07-29 10:32:58 +0200

    Sprite color routine changed
    The major issue when drawing elements so far was that blending was done using
    the alpha channel and the sprite was simply colourized using the default
    routines. This led to the issue of any displayed element being either dark or
    with the specific colour at max (as colours were simply multiplied) and then
    being blended with other colours below, which made it hard to actually specify
    how an element was supposed to look like.
    
    I modified the sprite shader to instead use a function that maps the sprites
    colours like this:
    
     * black --> black
    
     * 50% grey --> specified colour
    
     * white --> white
    
    Alpha works as before (original alpha * colour alpha).
    
    This means that we can now specifiy colours for a sprite, have solid black
    outlines present and also brighter, whiter parts inside there. This again
    means that I do not have to rely on the alpha channel for brighter colours
    (which was a hack anyway, as the whole thing would have turned out quite
    differently for a black background). I therefore redid the sprites to match the
    new colour scheme, which makes them seem more saturated now (which can,
    however, be adjusted easily).
    
    The result is that different field and droplet colours can be distinguished
    much more easily, which was the goal of this branch in the first place.
    
    Fixes #32

--- core/src/de/dfki/bioviz/ui/BioViz.java
+++ core/src/de/dfki/bioviz/ui/BioViz.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 import de.dfki.bioviz.messages.MessageCenter;
 import de.dfki.bioviz.parser.BioParser;
@@ -81,7 +82,7 @@ public class BioViz implements ApplicationListener {
 		float h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
+		batch = new SpriteBatch(1000, this.createDefaultShader());
 		
 		inputProcessor = new BioVizInputProcessor();
 		Gdx.input.setInputProcessor(inputProcessor);
@@ -274,5 +275,42 @@ public class BioViz implements ApplicationListener {
 		for (BioVizEvent listener : this.loadedFileListeners) {
 			listener.bioVizEvent();
 		}
+	}
+
+	static public ShaderProgram createDefaultShader () {
+		String vertexShader = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+				+ "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+				+ "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+				+ "uniform mat4 u_projTrans;\n" //
+				+ "varying vec4 v_color;\n" //
+				+ "varying vec2 v_texCoords;\n" //
+				+ "\n" //
+				+ "void main()\n" //
+				+ "{\n" //
+				+ "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+				+ "   v_color.a = v_color.a * (255.0/254.0);\n" //
+				+ "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+				+ "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+				+ "}\n";
+		String fragmentShader = "#ifdef GL_ES\n" //
+				+ "#define LOWP lowp\n" //
+				+ "precision mediump float;\n" //
+				+ "#else\n" //
+				+ "#define LOWP \n" //
+				+ "#endif\n" //
+				+ "varying LOWP vec4 v_color;\n" //
+				+ "varying vec2 v_texCoords;\n" //
+				+ "uniform sampler2D u_texture;\n" //
+				+ "void main()\n"//
+				+ "{\n" //
+				+ "  vec4 fctr = texture2D(u_texture, v_texCoords);" //
+				+ "  fctr = abs(fctr - 0.5) * -2.0 + 1.0;" //
+				+ "  gl_FragColor = v_color * fctr + texture2D(u_texture, v_texCoords) * (1.0 - fctr);\n" //
+				+ "  gl_FragColor.a = texture2D(u_texture, v_texCoords).a * v_color.a;"//
+				+ "}";
+
+		ShaderProgram shader = new ShaderProgram(vertexShader, fragmentShader);
+		if (shader.isCompiled() == false) throw new IllegalArgumentException("Error compiling shader: " + shader.getLog());
+		return shader;
 	}
 }
