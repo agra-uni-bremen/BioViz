1faaf62e21c5c80cf756486cdf1bff6df2906817
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2015-08-04 16:28:13 +0200

    Net now has public final members

--- core/src/de/dfki/bioviz/structures/Net.java
+++ core/src/de/dfki/bioviz/structures/Net.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  * Created by keszocze on 27.07.15.
  */
 public class Net {
-	private Point target;
-	private ArrayList<Source> sources=new ArrayList<Source>();
+	public final Point target;
+	public final ArrayList<Source> sources=new ArrayList<Source>();
 
 	public Net(ArrayList<Source> sources, Point target) {
 		this.target = target;
