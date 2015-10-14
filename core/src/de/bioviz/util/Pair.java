4d498c984c4d1b7ac21ae8d699cfa1c017393ef8
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2015-10-14 12:44:16 +0200

    Finish making Pair checkstyle conform

--- core/src/de/bioviz/util/Pair.java
+++ core/src/de/bioviz/util/Pair.java
@@ -1,14 +1,18 @@
+
 package de.bioviz.util;
 
 /**
- * Created by keszocze on 27.07.15.
- */
-
-/**
+ * A very simple tuple class.
+ *
+ * This class is necessary as, for no good reasons at all, Java does not come
+ * with any means of creating tuples.
+ *
  * @param <K>
  * 		Type for the first entry of the tuple
  * @param <V>
  * 		Type for the snd entry of the tuple
+ *
+ * @author Oliver Keszöcze
  */
 public class Pair<K, V> {
 
@@ -18,14 +22,28 @@ public class Pair<K, V> {
 	public final K fst;
 
 	/**
-	 * The snd entry of the tuple.
+	 * The second entry of the tuple.
 	 */
 	public final V snd;
 
-	public static <K, V> Pair<K, V> mkPair(final K first, final V second) {
-		return new Pair<K, V>(first, second);
+
+	/**
+	 *
+	 * @param fst Value of the first entry of the pair
+	 * @param snd Value of the second entry of the pair
+	 * @param <K> Type of the first entry of the pair
+	 * @param <V> Type of the second entry of the pair
+	 * @return Pair of type (K,V) with values (fst,snd)
+	 */
+	public static <K, V> Pair<K, V> mkPair(final K fst, final V snd) {
+		return new Pair<K, V>(fst, snd);
 	}
 
+	/**
+	 * @brief Creates a new pair containing the values provided as parameters.
+	 * @param fst Value of the first entry of the pair
+	 * @param snd Value of the second entry of the pair
+	 */
 	public Pair(final K fst, final V snd) {
 		this.fst = fst;
 		this.snd = snd;
@@ -39,8 +57,7 @@ public class Pair<K, V> {
 	 * @brief Checks equality of the pair with another object.
 	 * <p>
 	 * An object is treated as equals if it also is a pair and the values in the
-	 * first and snd position are equal as determined by their equals()
-	 * method.
+	 * first and snd position are equal as determined by their equals() method.
 	 */
 	@Override
 	public boolean equals(final Object o) {
@@ -61,12 +78,12 @@ public class Pair<K, V> {
 	 * @return The sum of the hashcodes, i.e. fst.hashCode()+snd.hashCode()
 	 */
 	public int hashCode() {
-		return fst.hashCode()+ snd.hashCode();
+		return fst.hashCode() + snd.hashCode();
 	}
 
 	/**
-	 * @return String representing the Pair of the form '(' + first + ',' +
-	 * snd + ')'
+	 * @return String representing the Pair of the form '(' + first + ',' + snd
+	 * + ')'
 	 */
 	public String toString() {
 		return "(" + fst + "," + snd + ")";
