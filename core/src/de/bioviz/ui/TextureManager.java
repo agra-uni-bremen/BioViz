e5d0fdeaef26676c8f723f2e2602c1e71567fda5
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2015-10-15 16:23:01 +0200

    Use the TextureManager :)

--- core/src/de/bioviz/ui/TextureManager.java
+++ core/src/de/bioviz/ui/TextureManager.java
@@ -13,16 +13,26 @@ import java.util.HashMap;
  */
 public class TextureManager {
 
-	private HashMap<TextureE, TextureRegion> textures;
+	private HashMap<TextureE, TextureRegion> textures = new HashMap<>();
 
-	private HashMap<TextureE, String> textureFileNames;
+	private HashMap<TextureE, String> textureFileNames =  new HashMap<>();
 
 	private String textureFolder;
 
 	TextureManager(String folder) {
-		textureFolder = folder;
+		setFolder(folder);
+
+
 	}
 
+	public void setFolder(String folder) {
+		textures.clear();
+
+		textureFolder = folder;
+		for (TextureE t: TextureE.values()) {
+			textureFileNames.put(t,textureFolder+"/"+t.toString()+".png");
+		}
+	}
 
 	public TextureRegion getTexture(TextureE texture) {
 
@@ -37,10 +47,7 @@ public class TextureManager {
 						"Texture " + texture + " has no associated filename");
 			}
 
-			String textureFilename = textureFolder + "/" +
-									 textureFileNames.get(texture);
-
-			Texture t = new Texture(Gdx.files.internal(textureFilename), true);
+			Texture t = new Texture(Gdx.files.internal(textureFileNames.get(texture)), true);
 			t.setFilter(TextureFilter.MipMapLinearLinear, TextureFilter.Linear);
 			TextureRegion region = new TextureRegion(t, 0, 0, t.getWidth(), t.getHeight());
 			textures.put(texture, region);
