f21c86f7d0250383dedce6150d16a4df98a6bece
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2015-10-16 08:29:26 +0200

    Add enum for boolean display options

---
+++ core/src/de/bioviz/ui/BDisplayOptions.java
@@ -0,0 +1,9 @@
+package de.bioviz.ui;
+
+/**
+ * @authoer Oliver Keszöcze
+ */
+public enum BDisplayOptions {
+	CellUsage, Adjacency, DropletIDs, FluidIDs, Pins, Droplets, Actuations,
+	SourceTargetIcons, SourceTargetIDs, Coordinates
+}
