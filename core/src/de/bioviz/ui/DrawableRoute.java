7280627d1ac7af5ccaa34016273c7aa144e677e8
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2015-11-10 17:19:27 +0100

    Desktop is nearly checkstyle compliant
    The only two checkstyle errors remaining are two method length violations, which I'm not too sure we should fix.
    Simply because the methods in question are
    
     * the method that sets up the left-hand panel with lots of very same-ish ui initialization methods and
     * the method that translates every single keycode from awt to libgdx codes.
    
    I also modified the right curly brace rule because I prefer them to be on the same line on case of else and catch statements.

--- core/src/de/bioviz/ui/DrawableRoute.java
+++ core/src/de/bioviz/ui/DrawableRoute.java
@@ -22,6 +22,10 @@ public class DrawableRoute extends DrawableSprite {
 		this.droplet = droplet;
 		super.addLOD(DEFAULT_LOD_THRESHOLD, TextureE.BlackPixel);
 	}
+	
+	public static void setTimesteps(final int value) {
+		timesteps = value;
+	}
 
 	@Override
 	public String generateSVG() {
