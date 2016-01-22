17dd8ae4ed1baf9b3cdfbd1b0d8ba582f6b80105
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-01-22 10:06:03 +0100

    Rectangle now stores its data in Points
    Also it computes the bottom left and top right corners

--- core/src/de/bioviz/structures/Rectangle.java
+++ core/src/de/bioviz/structures/Rectangle.java
@@ -7,60 +7,61 @@ import java.util.ArrayList;
  */
 public class Rectangle {
 
-    int x1,x2,y1,y2;
+	int x1, x2, y1, y2;
 
-    public Point fstCorner() {
-        return new Point(x1,y1);
-    }
-    public Point sndCorner() {
-        return new Point(x2,y2);
-    }
+	Point bottomLeft;
+	Point topRight;
 
-    public Rectangle(Point p1, Point p2) {
-        this(p1.fst,p1.snd,p2.fst,p2.snd);
-    }
+	public Point fstCorner() {
+		return new Point(x1, y1);
+	}
 
-    public Rectangle(int x1, int y1, int x2, int y2) {
-        this.x1=x1;
-        this.x2=x2;
-        this.y1=y1;
-        this.y2=y2;
-    }
+	public Point sndCorner() {
+		return new Point(x2, y2);
+	}
 
-    public boolean contains(Point p) {
-        return contains(p.fst, p.snd);
-    }
+	public Rectangle(Point p1, Point p2) {
+		this(p1.fst, p1.snd, p2.fst, p2.snd);
+	}
 
-    public boolean contains(int x, int y) {
-        int minX = Math.min(x1,x2);
-        int minY = Math.min(y1, y2);
-        int maxX = Math.max(x1,x2);
-        int maxY = Math.max(y1,y2);
+	public Rectangle(int x1, int y1, int x2, int y2) {
 
-        return minX <= x && x <= maxX && minY <= y && y <= maxY;
-    }
 
-    public ArrayList<Point> positions() {
-        int minX = Math.min(x1,x2);
-        int minY = Math.min(y1, y2);
-        int maxX = Math.max(x1,x2);
-        int maxY = Math.max(y1,y2);
+		int minX = Math.min(x1, x2);
+		int minY = Math.min(y1, y2);
+		int maxX = Math.max(x1, x2);
+		int maxY = Math.max(y1, y2);
 
-        ArrayList<Point> result = new ArrayList<Point>();
+		bottomLeft = new Point(minX, minY);
+		topRight = new Point(maxX, maxY);
 
-        for (int x = minX; x<=maxX; x++) {
-            for (int y = minY; y<= maxY; y++) {
-                result.add(new Point(x,y));
-            }
-        }
+	}
 
-        return result;
-    }
+	public boolean contains(Point p) {
+		return contains(p.fst, p.snd);
+	}
 
-    public String toString() {
-        return "Rect["+ new Point(x1,y1) + " " + new Point(x2,y2)+"]";
-    }
+	public boolean contains(int x, int y) {
 
+		return bottomLeft.fst <= x && x <= topRight.fst && bottomLeft.snd <= y && y <= topRight.snd;
+	}
+
+	public ArrayList<Point> positions() {
+
+		ArrayList<Point> result = new ArrayList<Point>();
+
+		for (int x = bottomLeft.fst; x <= topRight.fst; x++) {
+			for (int y = bottomLeft.fst; y <= topRight.snd; y++) {
+				result.add(new Point(x, y));
+			}
+		}
+
+		return result;
+	}
+
+	public String toString() {
+		return "Rect[" + bottomLeft + " " + topRight + "]";
+	}
 
 
 }
