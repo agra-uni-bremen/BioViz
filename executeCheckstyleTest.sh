e3b9e09fcd4704753160cc698a9b2222e7ff7bfc
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-01-22 10:33:03 +0100

    Added checkstyle execution script
    The idea is that the checkstyle build should fail if the number of checkstyle errors increases (not only if there are any, as there are currently too many)
    Currently, the number of errors is fixed in the file and it's not yet used in the CI.

---
+++ executeCheckstyleTest.sh
@@ -0,0 +1,15 @@
+maxCheckstyleErrors=5110
+
+./gradlew core:checkstyleMain > checkstyleOutput 2>&1
+wc -l checkstyleOutput > wcResult
+read errors filename < wcResult
+rm wcResult
+echo "found $errors checkstyle errors"
+rm ./checkstyleOutput
+if [ "$errors" -gt "$maxCheckstyleErrors" ]
+then
+	echo "Checkstyle FAILED"
+else
+	echo "Checkstyle SUCCEEDED"
+fi
+
