0b0fcb684226fc9401e4f3c256032a89138ab82f
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-01-26 15:16:59 +0100

    Fixed Actuation.java according to checkstyle guidelines.
    This included a package info for the whole structures package.
    Hope everyone's fine with my descriptions, I'll just keep going.

---
+++ core/src/de/bioviz/structures/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>This package provides the basic structures that are being visualized.</p>
+ * <p>While the de.bioviz.ui package contains structures that are supposed to be
+ * <i>drawn</i>, this packages contains the elements that represent the entities
+ * behind the drawable structures. Therefore, most DrawableSomething classes
+ * from the ui package reference an according structure from this package. Each
+ * DrawableDroplet e.g. <i>contains</i> a de.bioviz.structures.Droplet that
+ * represents its data.</p>
+ */
+package de.bioviz.structures;
