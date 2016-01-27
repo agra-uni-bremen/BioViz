3a8613becc24111cbc06d2f5918538dedec981ef
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-01-27 21:39:07 +0100

    Add a random number for the net's color to the Net class
    Admittedly, the current 'algorithm' for setting the seed is pretty
    primitive. Currently it is just a proof of concept that will be replaced
    by a more elaborate thingie later on.

--- core/src/de/bioviz/structures/Net.java
+++ core/src/de/bioviz/structures/Net.java
@@ -2,16 +2,28 @@ package de.bioviz.structures;
 
 import java.util.ArrayList;
 
+import java.util.Random;
+
 /**
  * Created by keszocze on 27.07.15.
+ *
+ * @author Oliver Keszöcze
  */
 public class Net {
 	public final Point target;
 	public final ArrayList<Source> sources = new ArrayList<Source>();
 
+
+	public final int color;
+
 	public Net(ArrayList<Source> sources, Point target) {
 		this.target = target;
 		this.sources.addAll(sources);
+		Random rnd = new Random();
+
+		// TODO be more sophisticated here ^^
+		rnd.setSeed(target.fst+target.snd);
+		color=rnd.nextInt();
 	}
 
 	public boolean containsDroplet(Droplet d) {
