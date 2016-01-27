d1eccf8ed4da904fdfc3e0651619bcc4c121c1ee
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-01-27 20:36:32 +0100

    Set log level for color changing from annyoing to ok

--- core/src/de/bioviz/ui/DrawableSprite.java
+++ core/src/de/bioviz/ui/DrawableSprite.java
@@ -204,7 +204,7 @@ public abstract class DrawableSprite implements Drawable {
 
 	public void setColor(Color color) {
 		if (!this.targetColor.equals(color)) {
-			logger.debug("setting colour to " + color);
+			logger.trace("setting colour to " + color);
 			originColor = this.currentColor;
 			this.targetColor = color;
 			Date d = new Date();
