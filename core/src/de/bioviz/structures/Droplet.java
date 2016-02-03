3e4c82963e30b79f0a0376b3c3ec5b197b22d415
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-02-03 21:19:21 +0100

    reorder code in Droplet class

--- core/src/de/bioviz/structures/Droplet.java
+++ core/src/de/bioviz/structures/Droplet.java
@@ -1,24 +1,56 @@
 package de.bioviz.structures;
 
-import java.util.Date;
-import java.util.Collections;
-import java.util.Vector;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.bioviz.ui.DrawableCircuit;
+import java.util.Date;
+import java.util.Vector;
 
+/**
+ * @author Oliver Keszöcze
+ *         <p>
+ *         Class that contains everything that is necessary to describe a DMFB
+ *         droplet. This includes basic facts such as a unique ID as well as
+ *         information about the movement on the chip.
+ */
 public class Droplet {
-	
-	static Logger logger = LoggerFactory.getLogger(Droplet.class);
-	
+
+	public float smoothX;
+	public float smoothY;
+
+
+	private boolean firstUpdate = true;
+
 	public static int movementTransitionDuration = 500;
-	
+
+	static Logger logger = LoggerFactory.getLogger(Droplet.class);
+
+	private Net net = null;
+
 	private Vector<Point> positions = new Vector<>();
 
-	private int id=0;
+	private int id = 0;
 	private int spawnTime = 1;
+
+	private float targetX;
+	private float targetY;
+	private float originX;
+	private float originY;
+
+	private float movementDelay = 4f;
+
+	private long movementTransitionStartTime = 0, movementTransitionEndTime
+			= 0;
+
+	public Droplet(int id) {
+		this.id = id;
+	}
+
+	public Droplet(int id, int spawnTime) {
+		this.id = id;
+		this.spawnTime = spawnTime;
+	}
+
 
 	public Net getNet() {
 		return net;
@@ -28,52 +60,30 @@ public class Droplet {
 		this.net = net;
 	}
 
-	private Net net = null;
-	
-	private float movementDelay = 4f;
-	
-	private long movementTransitionStartTime = 0, movementTransitionEndTime = 0;
 
 	public Vector<Point> getPositions() {
 		return positions;
 	}
 
 	/**
-	 *
 	 * @return Length of the route of this droplet.
 	 */
 	public int getRouteLength() {
 		return positions.size();
 	}
 
-	public float smoothX, smoothY;
 
-	private float targetX;
-	private float targetY;
-	private float originX;
-	private float originY;
-	
-	private boolean firstUpdate = true;
-
-	public Droplet(int id) {
-		this.id = id;
-	}
-	public Droplet(int id, int spawnTime) {
-		this.id = id;
-		this.spawnTime=spawnTime;
-	}
-	
 	public void addPosition(int x, int y) {
-		positions.add(new Point(x,y));
+		positions.add(new Point(x, y));
 	}
 
 	public void addPosition(Point p) {
 		positions.add(p);
 	}
-	
+
 	public Point getSafePositionAt(int t) {
 
-		int index = t-spawnTime;
+		int index = t - spawnTime;
 
 		if (positions.isEmpty()) {
 			return null;
@@ -90,37 +100,39 @@ public class Droplet {
 
 	public Point getPositionAt(int t) {
 
-		int index = t-spawnTime;
+		int index = t - spawnTime;
 
 		if (positions.isEmpty() || index < 0 || index >= positions.size()) {
 			return null;
 		}
 		return positions.get(index);
 	}
-	
+
 	public Point getFirstPosition() {
 		return positions.firstElement();
 	}
-	
+
 	public Point getLastPosition() {
 		return positions.lastElement();
 	}
 
-	
+
 	/**
-	 * Calculates the time at which the <i>next</i> step is performed
-	 * for this blob. If a blob moves at timestamps 0, 1, 5, 10, calling
-	 * this method with current=4 would return 5. Calling it with current=5
-	 * would return 10. If current is at or after the last value, the last
-	 * value is returned.
-	 * @param current the current time
+	 * Calculates the time at which the <i>next</i> step is performed for this
+	 * blob. If a blob moves at timestamps 0, 1, 5, 10, calling this method
+	 * with
+	 * current=4 would return 5. Calling it with current=5 would return 10. If
+	 * current is at or after the last value, the last value is returned.
+	 *
+	 * @param current
+	 * 		the current time
 	 * @return the timestamp at which the next step is performed.
 	 */
 	public long getNextStep(long current) {
 		long result = 0;
 
-		if (current > 0 && current < positions.size()-1) {
-			result = current+1;
+		if (current > 0 && current < positions.size() - 1) {
+			result = current + 1;
 		}
 		else {
 			result = positions.size();
@@ -129,7 +141,7 @@ public class Droplet {
 
 		return result;
 	}
-	
+
 	public void update() {
 		if (firstUpdate) {
 			smoothX = getTargetX();
@@ -138,15 +150,22 @@ public class Droplet {
 			originY = getTargetY();
 			firstUpdate = false;
 		}
-		
+
 		float totalProgress = 1;
 		if (movementTransitionStartTime != movementTransitionEndTime) {
-			float transitionProgress = Math.max(0, Math.min(1, (float)(new Date().getTime() - movementTransitionStartTime) / (float)(movementTransitionEndTime - movementTransitionStartTime)));
-			totalProgress = (float)(-(Math.pow((transitionProgress - 1), 4)) + 1);
+			float transitionProgress = Math.max(0, Math.min(1, (float) (
+					new Date().getTime() - movementTransitionStartTime) /
+															   (float) (
+																	   movementTransitionEndTime -
+																	   movementTransitionStartTime)));
+			totalProgress =
+					(float) (-(Math.pow((transitionProgress - 1), 4)) + 1);
 		}
-		
-		smoothX = this.originX * (1 - totalProgress) + this.targetX * totalProgress;
-		smoothY = this.originY * (1 - totalProgress) + this.targetY * totalProgress;
+
+		smoothX = this.originX * (1 - totalProgress) +
+				  this.targetX * totalProgress;
+		smoothY = this.originY * (1 - totalProgress) +
+				  this.targetY * totalProgress;
 	}
 
 	@Override
@@ -166,25 +185,29 @@ public class Droplet {
 
 	/**
 	 * Retrieves the last timestamp at which this droplet moves.
+	 *
 	 * @return the time at which the droplet's last movement is performed
-	 * @warning Returns 0 in case no positions (i.e. no path) is associated with this droplet
+	 * @warning Returns 0 in case no positions (i.e. no path) is associated
+	 * with
+	 * this droplet
 	 */
 	public int getMaxTime() {
-		return positions.size()+spawnTime-1;
+		return positions.size() + spawnTime - 1;
 	}
-	
+
 	public int getSpawnTime() {
 		return this.spawnTime;
 	}
 
 	@Override
 	public String toString() {
-		return "D["+id+"@"+spawnTime+"]";
+		return "D[" + id + "@" + spawnTime + "]";
 	}
 
 	public int getID() {
 		return this.id;
 	}
+
 	public float getTargetX() {
 		return targetX;
 	}
@@ -192,7 +215,7 @@ public class Droplet {
 	public float getTargetY() {
 		return targetY;
 	}
-	
+
 	public void setTargetPosition(float targetX, float targetY) {
 		if (this.targetX != targetX || this.targetY != targetY) {
 			originX = this.smoothX;
@@ -200,8 +223,9 @@ public class Droplet {
 			this.targetX = targetX;
 			this.targetY = targetY;
 			Date d = new Date();
-			this.movementTransitionStartTime = d.getTime(); 
-			this.movementTransitionEndTime = d.getTime() + movementTransitionDuration;
+			this.movementTransitionStartTime = d.getTime();
+			this.movementTransitionEndTime =
+					d.getTime() + movementTransitionDuration;
 		}
 	}
 
