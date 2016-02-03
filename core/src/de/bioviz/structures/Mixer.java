9a2e2321752da7d77f6f1185f9bc266c86dbbd32
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-02-03 21:15:13 +0100

    Add comments to Mixer class

--- core/src/de/bioviz/structures/Mixer.java
+++ core/src/de/bioviz/structures/Mixer.java
@@ -1,22 +1,44 @@
 package de.bioviz.structures;
 
-import de.bioviz.util.Pair;
-
 /**
- * Created by keszocze on 17.08.15.
+ * @author Oliver Keszöcze
  */
 public class Mixer {
 
+	/**
+	 * The unique ID of the mixer.
+	 */
 	public final int id;
+
+	/**
+	 * The rectangle defining the size of the mixer.
+	 *
+	 * Note that currently only the abstraction of a mixer is used. You can not
+	 * define a mixer to be a certain set of movements (e.g., a spiral).
+	 */
 	public final Rectangle positions;
+
+	/**
+	 * Stores the timing information on this mixer.
+	 */
 	public final Range timing;
 
-	public Mixer(int id, Rectangle pos, Range timing) {
+	/**
+	 * Standard constructor requiring every value to be present.
+	 * @param id Unique mixer id
+	 * @param pos The region, given as a rectangle, that is covered by the mixer
+	 * @param timing When the mixer is present on the chip
+	 */
+	public Mixer(final int id, final Rectangle pos, final Range timing) {
 		this.id = id;
 		this.positions = pos;
 		this.timing = timing;
 	}
 
+	/**
+	 * @return String representation in the form "Mixer+<ID>: <timing>
+	 * <position>"
+	 */
 	public String toString() {
 		return "Mixer " + this.id + ": " + timing + " " + positions;
 	}
