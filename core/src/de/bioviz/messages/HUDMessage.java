77635533c512885612c412670256d166b08faa17
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-02-25 10:25:44 +0100

    Messages now have a common base class for both, ordinary and HUD messages.

--- core/src/de/bioviz/messages/HUDMessage.java
+++ core/src/de/bioviz/messages/HUDMessage.java
@@ -2,19 +2,16 @@ package de.bioviz.messages;
 
 import com.badlogic.gdx.graphics.Color;
 
-public class HUDMessage {
-	public String message;
+public class HUDMessage extends MessageBase {
 	public float x;
 	public float y;
-	public Color color;
 	public float size;
 	public boolean hideWhenZoomedOut = true;
 
 	public HUDMessage(String message, float x, float y) {
-		this.message = message;
+		super(message);
 		this.x = x;
 		this.y = y;
-		this.color = Color.WHITE;
 		this.size = -1f;
 	}
 }
