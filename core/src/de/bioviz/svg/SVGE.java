f51f1162ec3ed7ebc19bb1ffccd007268887db06
Maximilian Luenert <malu@informatik.uni-bremen.de>
2016-03-01 12:53:41 +0100

    Remove SVGE.

--- core/src/de/bioviz/svg/SVGE.java
+++
@@ -1,9 +0,0 @@
-package de.bioviz.svg;
-
-/**
- * @author Oliver Keszöcze
- */
-public enum SVGE {
-	Droplet, Sink, Dispenser, Detector, Start, Target, AdjacencyMarker,
-	BlackPixel, Blockage, GridMarker, StepMarker
-}
