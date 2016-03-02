61565400c557d11fe033ba7bb08ee81cbc1e37b1
Maximilian Luenert <malu@informatik.uni-bremen.de>
2016-03-02 10:53:18 +0100

    Change SVGCoreCreator to be nearly checkstyle compliant.

--- core/src/de/bioviz/svg/SVGCoreCreator.java
+++ core/src/de/bioviz/svg/SVGCoreCreator.java
@@ -32,14 +32,18 @@ import java.nio.file.Path;
  */
 public class SVGCoreCreator {
 
-	/** logger */
-	private static final Logger logger = LoggerFactory.getLogger(SVGCoreCreator.class);
+	/** logger. */
+	private static final Logger logger =
+			LoggerFactory.getLogger(SVGCoreCreator.class);
 
-	/** path of the svg core folder */
+	/** path of the svg core folder. */
 	private String svgCoreFolder = "";
 
-	/** path of the image folder */
+	/** path of the image folder. */
 	private String baseFolder = "images";
+
+	/** the length of the color string without alpha */
+	private final int COLOR_DIGITS = 6;
 
 	/**
 	 * Creates a new SVGCoreCreator.
@@ -48,15 +52,15 @@ public class SVGCoreCreator {
 	}
 
 	/**
-	 * @param folder The name of the folder containing the theme, relative to the assets
-	 *               folder
+	 * @param folder The name of the folder containing the theme,
+	 *               relative to the assets folder
 	 * @brief Tells the manager where to find the svgs (i.e. .svg images).
 	 * <p>
 	 * The location is relative to the assets folder.
 	 * <p>
 	 * @warning The folder name must not begin or end with a slash!
 	 */
-	public void setFolder(String folder) {
+	public void setFolder(final String folder) {
 		svgCoreFolder = folder;
 	}
 
@@ -64,10 +68,11 @@ public class SVGCoreCreator {
 	 * Return the svg core without color.
 	 *
 	 * @param type The type of the core.
-	 * @return
+	 * @return the svg code for the given type as a string
 	 */
-	private String getSVGCode(TextureE type) {
-		String svgCoreFile = baseFolder + "/" + svgCoreFolder + "/" + type + ".plain.svg";
+	private String getSVGCode(final TextureE type) {
+		String svgCoreFile =
+				baseFolder + "/" + svgCoreFolder + "/" + type + ".plain.svg";
 
 		logger.debug("[SVG] Loading SVG core for {}", svgCoreFile);
 
@@ -75,23 +80,24 @@ public class SVGCoreCreator {
 		String svgCore = "";
 		try {
 			svgCore = new String(Files.readAllBytes(svgCoreFilePath));
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 		return svgCore;
 	}
 
 	/**
-	 * Returns a string containing the svg core data with the given fill and stroke color.
-	 *
-	 * TODO Better error handling.
+	 * Returns a string containing the svg core data with the given
+	 * fill and stroke color.
 	 *
 	 * @param type        The type of the core.
 	 * @param fillColor   The fill color.
 	 * @param strokeColor The stroke color.
 	 * @return String containing svg core data.
 	 */
-	public String getSVGCode(TextureE type, Color fillColor, Color strokeColor) {
+	public String getSVGCode(final TextureE type, final Color fillColor,
+													 final Color
+			strokeColor) {
 		String uncoloredCore = getSVGCode(type);
 		String coloredCore = uncoloredCore;
 
@@ -117,19 +123,22 @@ public class SVGCoreCreator {
 				return "";
 			}
 
-			if(fillColor != null) {
-				logger.debug("[SVG] Changing fillColor to {}.", fillColor.toString().substring(0, 6));
+			if (fillColor != null) {
+				logger.debug("[SVG] Changing fillColor to {}.",
+						fillColor.toString().substring(0, COLOR_DIGITS));
 				// set new id for this node if there is a fillColor
 					if (group.getNodeType() == Node.ELEMENT_NODE) {
 						Element elem = (Element) group;
-						elem.setAttribute("id", type.toString() + "-" + fillColor.toString().substring(0, 6));
+						elem.setAttribute("id", type.toString() + "-" +
+								fillColor.toString().substring(0, COLOR_DIGITS));
 					}
 			}
-			if(strokeColor != null) {
-				logger.debug("[SVG] Changing strokeColor to {}.", strokeColor.toString().substring(0, 6));
+			if (strokeColor != null) {
+				logger.debug("[SVG] Changing strokeColor to {}.",
+						strokeColor.toString().substring(0, COLOR_DIGITS));
 			}
 
-			if(fillColor != null || strokeColor != null) {
+			if (fillColor != null || strokeColor != null) {
 				switch (type) {
 					case Dispenser:
 					case Sink:
@@ -138,40 +147,49 @@ public class SVGCoreCreator {
 					case Target:
 					case GridMarker:
 						// change fillColor of the rectangle
-						setStyleForElement((Element) group, "rect", fillColor, strokeColor);
+						setStyleForElement((Element) group, "rect",
+								fillColor, strokeColor);
 						break;
 					case Blockage:
 						// change fillColor of the rectangle
-						setStyleForElement((Element) group, "rect", fillColor, strokeColor);
+						setStyleForElement((Element) group, "rect",
+								fillColor, strokeColor);
 						// change fillColor of the circle
-						setStyleForElement((Element) group, "circle", fillColor, strokeColor);
+						setStyleForElement((Element) group, "circle",
+								fillColor, strokeColor);
 
 						break;
 					case Droplet:
 						// change fillColor of the first path element
-						setStyleForElement((Element) group, "path", fillColor, strokeColor);
+						setStyleForElement((Element) group, "path",
+								fillColor, strokeColor);
 						break;
 					case AdjacencyMarker:
 						break;
 					case StepMarker:
 						// change fillColor of the first path element
-						setStyleForElement((Element) group, "path", fillColor, strokeColor);
+						setStyleForElement((Element) group, "path",
+								fillColor, strokeColor);
+						break;
+					default:
 						break;
 				}
 			}
 
 			coloredCore = getGroupFromDocument(doc);
 
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (TransformerException e) {
-			e.printStackTrace();
+		} catch (final UnsupportedEncodingException e) {
+			logger.error("[SVG] XML uses unknown encoding.");
+			logger.error(e.getMessage());
+		} catch (final IOException e) {
+			logger.error("[SVG] Could not read the xml.");
+			logger.error(e.getMessage());
+		} catch (final SAXException e) {
+			logger.error("[SVG] Error while parsing xml.");
+			logger.error(e.getMessage());
+		} catch (final ParserConfigurationException e) {
+			logger.error("[SVG] Error in xml parser configuration.");
+			logger.error(e.getMessage());
 		}
 
 		return coloredCore;
@@ -180,12 +198,14 @@ public class SVGCoreCreator {
 	/**
 	 * Sets the style tag for the first tag occurence.
 	 *
-	 * @param element
-	 * @param tagName
-	 * @param fillColor
-	 * @param strokeColor
+	 * @param element xml top node
+	 * @param tagName the name of the tag to modify
+	 * @param fillColor the new fillColor
+	 * @param strokeColor the new strokeColor
 	 */
-	private void setStyleForElement(Element element, String tagName, Color fillColor, Color strokeColor){
+	private void setStyleForElement(final Element element,
+																	final String tagName, final Color fillColor,
+																	final Color strokeColor) {
 		NodeList elements = element.getElementsByTagName(tagName);
 		if (elements.getLength() > 0) {
 			Node node = elements.item(0);
@@ -193,12 +213,14 @@ public class SVGCoreCreator {
 				Element elem = (Element) node;
 				String style = elem.getAttribute("style");
 				String styleAfter = "";
-				for (String split : style.split(";")) {
+				for (final String split : style.split(";")) {
 					String styleType = split.split(":")[0];
-					if (styleType.equals("fill") && fillColor != null) {
-						styleAfter += styleType + ":#" + fillColor.toString().substring(0, 6) + ";";
-					} else if (styleType.equals("stroke") && strokeColor != null) {
-						styleAfter += styleType + ":#" + strokeColor.toString().substring(0, 6) + ";";
+					if ("fill".equals(styleType) && fillColor != null) {
+						styleAfter +=	styleType + ":#" +
+								fillColor.toString().substring(0, COLOR_DIGITS) + ";";
+					} else if ("stroke".equals(styleType) && strokeColor != null) {
+						styleAfter += styleType + ":#" +
+								strokeColor.toString().substring(0, COLOR_DIGITS) + ";";
 					} else {
 						styleAfter += split + ";";
 					}
@@ -213,19 +235,28 @@ public class SVGCoreCreator {
 	 * Create string from xml representation.
 	 *
 	 * @param doc The xml document.
-	 * @return String representing the xml document.
-	 * @throws TransformerException
+	 * @return String representing the xml document or null if the
+	 * transformation failed.
 	 */
-	private String getGroupFromDocument(Document doc) throws TransformerException {
-		TransformerFactory tFactory = TransformerFactory.newInstance();
-		Transformer transformer = tFactory.newTransformer();
-		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	private String getGroupFromDocument(final Document doc) {
+		TransformerFactory tFactory;
+		Transformer transformer;
+		StringWriter writer;
+		try {
+			tFactory = TransformerFactory.newInstance();
+			transformer = tFactory.newTransformer();
 
-		DOMSource source = new DOMSource(doc.getElementsByTagName("g").item(0));
-		StringWriter writer = new StringWriter();
-		StreamResult result = new StreamResult(writer);
-		transformer.transform(source, result);
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+			DOMSource source = new DOMSource(doc.getElementsByTagName("g").item(0));
+			writer = new StringWriter();
+			StreamResult result = new StreamResult(writer);
+			transformer.transform(source, result);
+		} catch (final TransformerException e) {
+			logger.error("[SVG] Creating string from XML failed.");
+			return null;
+		}
 
 		return writer.toString();
 	}
