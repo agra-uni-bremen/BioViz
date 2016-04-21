55868acedcfdcfd11d91c21d5e2ee6fc9f632e45
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-04-21 11:50:25 +0200

    Do some documentation in BiochipField

--- core/src/de/bioviz/structures/BiochipField.java
+++ core/src/de/bioviz/structures/BiochipField.java
@@ -1,7 +1,5 @@
 package de.bioviz.structures;
 
-import de.bioviz.ui.BioViz;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,6 +42,7 @@ public class BiochipField {
 
 	/**
 	 * Checks whether this field is the destination of a net.
+	 *
 	 * @return true if the field is the destination of a net.
 	 */
 	public boolean isTarget() {
@@ -52,6 +51,7 @@ public class BiochipField {
 
 	/**
 	 * Checks whether this field is the source of a net.
+	 *
 	 * @return true if the field is the source of a net.
 	 */
 	public boolean isSource() {
@@ -75,36 +75,81 @@ public class BiochipField {
 	// field the fluid is removed from or dispensed to
 	Direction direction = null;
 
-	// if the field is a dispenser, this variable stores the fluid type that
-	// is dispensed
+	/**
+	 * The fluid ID that is dispensed from this field.
+	 * <p>
+	 * This variable only has a meaning if the field is a dispenser.
+	 */
 	public int fluidID = 0;
 
 
+	/**
+	 * Sets the time, this field is blocked.
+	 *
+	 * @param blockage
+	 * 		The {@link Range} describing the time the field is blocked.
+	 */
 	public void attachBlockage(Range blockage) {
 		this.blockage = blockage;
 	}
 
+	/**
+	 * Checks whether the field is blocked at the specified time step.
+	 *
+	 * @param timeStep The time step to check.
+	 * @return true if the field is blocked at timeStep, false otherwise.
+	 */
 	public boolean isBlocked(int timeStep) {
-		if (blockage == null) {
-			return false;
+
+		if (isPotentiallyBlocked()) {
+			return blockage.inRange(timeStep);
 		}
 		else {
-			return blockage.inRange(timeStep);
+			return false;
 		}
 	}
 
+	/**
+	 * Checks whether this field might be blocked at some point.
+	 * <p>
+	 * This method only checks whether there is a blockage associated with this
+	 * field. As no time step is specified, this method only provides the
+	 * information that this field may be blocked at some point in time.
+	 *
+	 * @return True if there is a blockage associated with this field.
+	 */
+	public boolean isPotentiallyBlocked() {
+		return !(blockage == null);
+	}
+
+
+	/**
+	 * Checks whether the field is actuated at the specified time step.
+	 * <p>
+	 * The order in which the field is checked for its actuation is as follows:
+	 * 1. If there is a pin assignment, the actuation state of the pin is used.
+	 * 2. If there is an actuation vector assigned to this field, its value is
+	 * used. 3. If there is a droplet on top of this field, the field is
+	 * regarded as being actuated.
+	 * <p>
+	 * Note that any of the above steps determined the actuation state of the
+	 * field, the other checks are not performed. This means that if the field
+	 * has an assigned pin that is never actuated, even the presence of a
+	 * droplet on top of the field does not make this field actuated.
+	 * <p>
+	 * The exception from this rule is that the presence of a mixer always sets
+	 * the status to actuated.
+	 *
+	 * @param timeStep
+	 * 		The time step to check for actuation of this field.
+	 * @return true if the field is actuated, false otherwise.
+	 */
 	public boolean isActuated(int timeStep) {
 
 		Biochip circ = parent;
 		Actuation act = Actuation.OFF;
 
-		// TODO document that pin actuations win over cell actuations
-
 		if (pin != null && !circ.pinActuations.isEmpty()) {
-			logger.trace("circ.pinActuations.isEmpty: {}",
-						 circ.pinActuations.isEmpty());
-			logger.trace("timeStep: {} actuationVector {}", timeStep,
-						 circ.pinActuations.get(pin.pinID));
 			ActuationVector vec = circ.pinActuations.get(pin.pinID);
 			if (vec != null) {
 				act = vec.get(timeStep - 1);
@@ -119,7 +164,7 @@ public class BiochipField {
 			}
 		}
 
-		for (Mixer m: mixers) {
+		for (Mixer m : mixers) {
 			if (m.positions.contains(pos) && m.timing.inRange(timeStep)) {
 				act = Actuation.ON;
 			}
@@ -128,9 +173,6 @@ public class BiochipField {
 		return act == Actuation.ON;
 	}
 
-	public boolean isPotentiallyBlocked() {
-		return !(blockage == null);
-	}
 
 	public void setSink(final Direction removeTo) {
 		isDispenser = false;
@@ -174,20 +216,20 @@ public class BiochipField {
 	public enum NetBorder {
 		North, East, South, West;
 	}
-	
+
 	public EnumSet<NetBorder> getBorderType(Net partOf) {
 		EnumSet<NetBorder> result = EnumSet.noneOf(NetBorder.class);
-		
+
 		if (partOf.containsField(this)) {
 			BiochipField top, bottom, left, right;
 			int x = this.x();
 			int y = this.y();
-			
+
 			right = parent.getFieldAt(new Point(x + 1, y));
 			left = parent.getFieldAt(new Point(x - 1, y));
 			top = parent.getFieldAt(new Point(x, y + 1));
 			bottom = parent.getFieldAt(new Point(x, y - 1));
-			
+
 			if (!partOf.containsField(top)) {
 				result.add(NetBorder.North);
 			}
@@ -201,7 +243,7 @@ public class BiochipField {
 				result.add(NetBorder.East);
 			}
 		}
-		
+
 		return result;
 	}
 
