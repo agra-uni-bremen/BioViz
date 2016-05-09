agra-uni-bremen/BioViz

commit 4dee34078b4864e4f859c6db1dc3c951c308d847

Merge of branch display_options_class
This merge actually was a pain in the ass. Hopefully nothing is broken.

core/src/de/bioviz/ui/DrawableField.java
@@ -3,6 +3,7 @@ package de.bioviz.ui;
 import com.badlogic.gdx.graphics.Color;
 import de.bioviz.structures.BiochipField;
 import de.bioviz.structures.Mixer;
+import de.bioviz.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +22,6 @@ public class DrawableField extends DrawableSprite {
 	static final Color mixerDefaultColor = Colors.mixerColor;
 	static final Color blockedColor = Colors.blockedColor;
 
-	private boolean drawSink = false;
-	private boolean drawBlockage = false;
-	private boolean drawDetector = false;
 
 	//private DrawableSprite adjacencyOverlay;
 
@@ -38,15 +36,24 @@ public class DrawableField extends DrawableSprite {
 	}
 
 
-	@Override
-	public void draw() {
+	public DisplayValues getDisplayValues() {
+
+
+		Pair<String, TextureE> msgTexture = getMsgTexture();
+
+		Color color = getColor();
+
+		return new DisplayValues(color, msgTexture.fst, msgTexture.snd);
+	}
+
+	public Pair<String, TextureE> getMsgTexture() {
 
 		String fieldHUDMsg = null;
 		DrawableCircuit circ = parentCircuit;
 		int t = circ.currentTime;
 		float xCoord = circ.xCoordOnScreen(field.x());
 		float yCoord = circ.yCoordOnScreen(field.y());
-
+		TextureE texture = TextureE.GridMarker;
 		this.x = xCoord;
 		this.y = yCoord;
 		this.scaleX = circ.smoothScaleX;
@@ -54,28 +61,35 @@ public class DrawableField extends DrawableSprite {
 
 
 		// TODO what happens if some of these options overlap?
-		// Right now only the first occurrence according the order below is taken. This might not be what is intended
+		// Right now only the first occurrence according the order below is
+		// taken. This might not be what is intended
 		// In general, a detector, for example, is a very valid routing target
-		if (this.field.isSink && !drawSink) {
-			this.addLOD(Float.MAX_VALUE, TextureE.Sink);
-			drawSink = true;
-		} else if (this.field.isDispenser) {
-			this.addLOD(Float.MAX_VALUE, TextureE.Dispenser);
-			fieldHUDMsg = Integer.toString(field.fluidID);
-		} else if (this.field.isPotentiallyBlocked() && !drawBlockage) {
-			this.addLOD(Float.MAX_VALUE, TextureE.Blockage);
-			drawBlockage = true;
-		} else if (this.field.getDetector() != null && !drawDetector) {
-			this.addLOD(Float.MAX_VALUE, TextureE.Detector);
-			drawDetector = true;
-		} else if (!this.field.source_ids.isEmpty()) {
+		if (this.field.isSink && circ.displayOptions.getOption(BDisplayOptions.SinkIcon)) {
+			texture = TextureE.Sink;
+		}
+		else if (this.field.isDispenser) {
+			if (circ.displayOptions.getOption(BDisplayOptions.DispenserIcon)) {
+				texture = TextureE.Dispenser;
+			}
+			if (circ.displayOptions.getOption(BDisplayOptions.DispenserID)) {
+				fieldHUDMsg = Integer.toString(field.fluidID);
+			}
+		}
+		else if (this.field.isPotentiallyBlocked()) {
+			texture = TextureE.Blockage;
+		}
+		else if (this.field.getDetector() != null &&
+				 circ.displayOptions.getOption(BDisplayOptions.DetectorIcon)) {
+			texture = TextureE.Detector;
+		}
+		else if (!this.field.source_ids.isEmpty()) {
 			if (circ.displayOptions.getOption(BDisplayOptions
 													  .SourceTargetIcons)) {
-				this.addLOD(Float.MAX_VALUE, TextureE.Start);
-			} else {
-				this.addLOD(Float.MAX_VALUE, TextureE.GridMarker);
+				texture = TextureE.Start;
 			}
-			if (circ.displayOptions.getOption(BDisplayOptions.SourceTargetIDs)) {
+
+			if (circ.displayOptions.getOption(
+					BDisplayOptions.SourceTargetIDs)) {
 				ArrayList<Integer> sources = this.field.source_ids;
 				fieldHUDMsg = sources.get(0).toString();
 				if (sources.size() > 1) {
@@ -84,13 +98,14 @@ public class DrawableField extends DrawableSprite {
 					}
 				}
 			}
-		} else if (!this.field.target_ids.isEmpty()) {
-			if (circ.displayOptions.getOption(BDisplayOptions.SourceTargetIcons)) {
-				this.addLOD(Float.MAX_VALUE,TextureE.Target);
-			} else {
-				this.addLOD(Float.MAX_VALUE, TextureE.GridMarker);
+		}
+		else if (!this.field.target_ids.isEmpty()) {
+			if (circ.displayOptions.getOption(
+					BDisplayOptions.SourceTargetIcons)) {
+				texture = TextureE.Target;
 			}
-			if (circ.displayOptions.getOption(BDisplayOptions.SourceTargetIDs)) {
+			if (circ.displayOptions.getOption(
+					BDisplayOptions.SourceTargetIDs)) {
 				ArrayList<Integer> targets = this.field.target_ids;
 				fieldHUDMsg = targets.get(0).toString();
 				if (targets.size() > 1) {
@@ -103,58 +118,68 @@ public class DrawableField extends DrawableSprite {
 
 
 		// note: this overwrites any previous message
-		// TODO we really need some kind of mechanism of deciding when to show what
+		// TODO we really need some kind of mechanism of deciding when to show
+		// what
 		if (circ.displayOptions.getOption(BDisplayOptions.Pins)) {
 			if (this.field.pin != null) {
 				fieldHUDMsg = Integer.toString(this.field.pin.pinID);
 			}
 		}
 
-		if (fieldHUDMsg != null) {
-			parentCircuit.parent.mc.addHUDMessage(this.hashCode(), fieldHUDMsg, xCoord, yCoord);
-		} else {
-			parentCircuit.parent.mc.removeHUDMessage(this.hashCode());
-		}
+		return Pair.mkPair(fieldHUDMsg, texture);
+	}
+
+	public Color getColor() {
 
 
 		int colorOverlayCount = 0;
 		/*
-		We need to create a copy of the fieldEmptyColor as that value is final and thus can not be modified.
+		We need to create a copy of the fieldEmptyColor as that value is final
+		 and thus can not be modified.
 		If that value is unchangeable, the cells all stay white
 		 */
 		Color result = new Color(Colors.fieldEmptyColor);
 
-		if (field.isBlocked(circ.currentTime)) {
+		if (field.isBlocked(parentCircuit.currentTime)) {
 			result.add(blockedColor);
 			colorOverlayCount++;
 		}
 
 
-		if (circ.displayOptions.getOption(BDisplayOptions.CellUsage)) {
-			// TODO clevere Methode zum Bestimmen der Farbe wählen (evtl. max Usage verwenden)
+		if (parentCircuit.displayOptions.getOption(BDisplayOptions
+														   .CellUsage)) {
+			// TODO clevere Methode zum Bestimmen der Farbe wählen (evtl. max
+			// Usage verwenden)
 			float scalingFactor = 4f;
 
 			result.add(new Color(0, this.field.usage / scalingFactor, 0, 0));
 			++colorOverlayCount;
 		}
 
-		if (circ.displayOptions.getOption(BDisplayOptions.Actuations)) {
+
+		int t = parentCircuit.currentTime;
+		if (parentCircuit.displayOptions.getOption(
+				BDisplayOptions.Actuations)) {
 			if (field.isActuated(t)) {
 				result.add(Colors.actautedColor);
 				++colorOverlayCount;
 			}
 		}
 
-		// TODO why do we only add something if the count is zero? Save computation time?
-		// nope it seems that the cell usage is supposed to override the other overlays
+		// TODO why do we only add something if the count is zero? Save
+		// computation time?
+		// nope it seems that the cell usage is supposed to override the other
+		// overlays
 		if (colorOverlayCount == 0) {
 			if (this.field.isSink) {
 				result.add(sinkDefaultColor);
 				colorOverlayCount++;
-			} else if (this.field.isDispenser) {
+			}
+			else if (this.field.isDispenser) {
 				result.add(sourceDefaultColor);
 				colorOverlayCount++;
-			} else {
+			}
+			else {
 				result.add(fieldDefaultColor);
 				colorOverlayCount++;
 			}
@@ -168,7 +193,9 @@ public class DrawableField extends DrawableSprite {
 			}
 		}
 
-		if (circ.displayOptions.getOption(BDisplayOptions.Adjacency) && circ.data.getAdjacentActivations().contains(this.field)) {
+		if (parentCircuit.displayOptions.getOption(BDisplayOptions
+														   .Adjacency) &&
+			parentCircuit.data.getAdjacentActivations().contains(this.field)) {
 			result.add(0.5f, -0.5f, -0.5f, 0);
 		}
 
@@ -176,7 +203,21 @@ public class DrawableField extends DrawableSprite {
 
 		result.clamp();
 
-		setColor(result);
+
+		return result;
+	}
+
+	@Override
+	public void draw() {
+
+
+		DisplayValues vals = getDisplayValues();
+
+		displayText(vals.msg);
+		this.addLOD(Float.MAX_VALUE, vals.texture);
+
+
+		setColor(vals.color);
 
 		super.draw();
 	}

core/assets/examples/detector_test.bio
@@ -1,0 +1,8 @@
+grid
+(1,1) (2,2)
+end
+
+
+detectors
+(1,2)
+end

core/src/de/bioviz/ui/BDisplayOptions.java
@@ -5,5 +5,6 @@ package de.bioviz.ui;
  */
 public enum BDisplayOptions {
 	CellUsage, Adjacency, DropletIDs, FluidIDs, Pins, Droplets, Actuations,
-	SourceTargetIcons, SourceTargetIDs, Coordinates, ColorfulRoutes
+	SourceTargetIcons, SourceTargetIDs, Coordinates, DispenserID, DispenserIcon,
+	SinkIcon, DetectorIcon, ColorfulRoutes
 }

core/src/de/bioviz/ui/BioVizInputProcessor.java
@@ -51,10 +51,29 @@ public class BioVizInputProcessor implements InputProcessor {
 			if (ctrl) {
 				parentViz.callSaveFileListeners();
 			}
-		} else if (keycode == Keys.A) {
-			parentViz.currentCircuit.displayOptions.toggleOption(
-					BDisplayOptions.Adjacency);
+		} 
+		else if (keycode == Keys.A) {
+			if (ctrl) {
+				parentViz.currentCircuit.displayOptions.toggleOption(
+						BDisplayOptions.Adjacency);
+			}
+			if (shift) {
+				parentViz.currentCircuit.displayOptions.toggleOption(
+						BDisplayOptions.DetectorIcon);
+			}
+
 		}
+		else if (keycode == Keys.D) {
+			if (ctrl) {
+				parentViz.currentCircuit.displayOptions.toggleOption(
+						BDisplayOptions.DispenserID);
+			}
+			if (shift) {
+				parentViz.currentCircuit.displayOptions.toggleOption(
+						BDisplayOptions.DispenserIcon);
+			}
+		}
+
 		else if (keycode == Keys.RIGHT || keycode == Keys.UP) {
 			parentViz.currentCircuit.nextStep();
 		}
@@ -65,11 +84,7 @@ public class BioVizInputProcessor implements InputProcessor {
 			if (ctrl) {
 				parentViz.callCloseFileListeners();
 			}
-		}
-		else if (keycode == Keys.R) {
-			parentViz.currentCircuit.displayOptions.toggleOption(BDisplayOptions.ColorfulRoutes);
-		}
-		else if (keycode == Keys.C){
+		} else if (keycode == Keys.C){
 			if (ctrl) {
 				parentViz.currentCircuit.displayOptions.toggleOption(
 						BDisplayOptions.Coordinates);

core/src/de/bioviz/ui/DisplayOptions.java
@@ -20,7 +20,10 @@ public class DisplayOptions {
 		setOption(BDisplayOptions.SourceTargetIcons,true);
 		setOption(BDisplayOptions.SourceTargetIDs,true);
 		setOption(BDisplayOptions.Droplets,true);
-		setOption(BDisplayOptions.Coordinates,true);
+		//setOption(BDisplayOptions.Coordinates,true);
+		setOption(BDisplayOptions.DispenserIcon,true);
+		setOption(BDisplayOptions.DispenserID,true);
+		setOption(BDisplayOptions.DetectorIcon,true);
 	}
 
 	public boolean getOption(BDisplayOptions opt) {

core/src/de/bioviz/ui/DisplayValues.java
@@ -1,0 +1,32 @@
+package de.bioviz.ui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ *
+ * @brief Class for storing the information about how a drawable should be drawn
+ *
+ * @author Oliver Keszöcze
+ */
+public class DisplayValues {
+
+	//! The color of the drawable
+	public final Color color;
+	//! The message displayed above the drawable
+	public final String msg;
+	//! The texture of the drawable
+	public final TextureE texture;
+
+	/**
+	 *
+	 * @param color The color of the drawable
+	 * @param msg The message displayed above the drawable
+	 * @param texture The texture of the drawable
+	 */
+	public DisplayValues(final Color color, final String msg, final TextureE texture) {
+		this.color = color;
+		this.msg = msg;
+		this.texture = texture;
+	}
+}

commit 8f7377f3b28512c4d99e85f38aaf85e013fac5a5

Fixed dispenser drawing
@keszocze this should fix this whole dispenser drawing thing.
The issue was introduced in 4dee34078b4864e4f859c6db1dc3c951c308d847 when
you added the getMsgTexture() method to the DrawableField class.
The problem is that you refactored the draw() method to contain less code,
which was instead shifted to getMsgTexture(), including several instructions
that set the coordinates of the object.
This may have been considered a bad smell (as a get-method shouldn't alter
an internal state) but didn't matter as all draw() calls also called that
getter.
When you now overwrote the getter with a custom function for the
DrawableDispenser you didn't add a call to the super-method, thus resulting
in the coordinates of the field remaining unset.

Refs #151

core/src/de/bioviz/ui/DrawableDispenser.java
@@ -42,7 +42,6 @@ public class DrawableDispenser extends DrawableField {
 
 
 		fieldHUDMsg = String.join(" - ", msgs);
-		System.out.println("Correct getMsgTextre()");
 		return Pair.mkPair(fieldHUDMsg,texture);
 	}
 }

core/src/de/bioviz/ui/DrawableField.java
@@ -98,15 +98,7 @@ public class DrawableField extends DrawableSprite {
 	public Pair<String, TextureE> getMsgTexture() {
 
 		String fieldHUDMsg = null;
-		DrawableCircuit circ = getParentCircuit();
-		int t = circ.getCurrentTime();
-		float xCoord = circ.xCoordOnScreen(getField().x());
-		float yCoord = circ.yCoordOnScreen(getField().y());
 		TextureE texture = TextureE.GridMarker;
-		this.setX(xCoord);
-		this.setY(yCoord);
-		this.setScaleX(circ.getSmoothScale());
-		this.setScaleY(circ.getSmoothScale());
 
 
 		// TODO what happens if some of these options overlap?
@@ -158,6 +150,7 @@ public class DrawableField extends DrawableSprite {
 		}
 
 
+		DrawableCircuit circ = getParentCircuit();
 		// note: this overwrites any previous message
 		// TODO we really need some kind of mechanism of deciding when to show
 		// what
@@ -380,6 +373,15 @@ public class DrawableField extends DrawableSprite {
 
 		displayText(vals.getMsg());
 		setColor(vals.getColor());
+
+		DrawableCircuit circ = getParentCircuit();
+		int t = circ.getCurrentTime();
+		float xCoord = circ.xCoordOnScreen(getField().x());
+		float yCoord = circ.yCoordOnScreen(getField().y());
+		this.setX(xCoord);
+		this.setY(yCoord);
+		this.setScaleX(circ.getSmoothScale());
+		this.setScaleY(circ.getSmoothScale());
 
 		// this call is actually necessary to draw any textures at all!
 		this.addLOD(Float.MAX_VALUE, vals.getTexture());