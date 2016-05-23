ed9b9efda4ab2300d48b673d5992ecfbb95d6524
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-05-23 15:32:32 +0200

    Added comments

--- core/src/de/bioviz/ui/BioVizSpriteBatch.java
+++ core/src/de/bioviz/ui/BioVizSpriteBatch.java
@@ -14,27 +14,71 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
+/**
+ * This class serves as a wrapper around the original libgdx sprite batch but
+ * adds depth sorting functionality.
+ *
+ * As the original sprite batch simply takes all draw calls and aggregates them
+ * to be executed in the same order they were placed in, ordering all elements
+ * as desired at some point got too messy to be done via ordering the calls.
+ * This class therefore collects them to be sorted and executed via a given
+ * priority value.
+ *
+ * @author jannis
+ *
+ */
 public class BioVizSpriteBatch {
+	/**
+	 * The original sprite batch to draw things in.
+	 */
+	private SpriteBatch sb;
 
+	/**
+	 * This field collects all draw calls and their associated priority.
+	 */
 	private ArrayList<Pair<Float, Runnable>> drawCalls =
 			new ArrayList<Pair<Float, Runnable>>();
-	private SpriteBatch sb;
+
+	/**
+	 * Creates a new batch to draw elements with.
+	 * @param sb the original libgdx batch to be wrapped.
+	 */
 	public BioVizSpriteBatch(SpriteBatch sb) {
 		this.sb = sb;
 	}
 
+	/**
+	 * Disposes this batch.
+	 */
 	public void dispose() {
 		sb.dispose();
 	}
 
+	/**
+	 * Sets a new projection matrix by calling the wrapped batch's
+	 * setProjectionMatrix method.
+	 * @param matrix the matrix to be used, retrieve from the camera.
+	 */
 	public void setProjectionMatrix(Matrix4 matrix) {
 		sb.setProjectionMatrix(matrix);
 	}
 
+	/**
+	 * Draws a sprite using a given matrix.
+	 * This sets the depth/priority value to 0.
+	 * @param s the sprite to be drawn.
+	 * @param m the matrix to transform the sprite with.
+	 */
 	public void draw(Sprite s, Matrix4 m) {
 		draw(s, m, 0);
 	}
 
+	/**
+	 * Draws a sprite using a given matrix at a given depth.
+	 * @param s the sprite to be drawn.
+	 * @param m the matrix to use.
+	 * @param z the depth value of the sprite.
+	 */
 	public void draw(Sprite s, Matrix4 m, float z) {
 		float[] vertices = s.getVertices().clone();
 		Texture tex = s.getTexture();
@@ -53,10 +97,17 @@ public class BioVizSpriteBatch {
 				));
 	}
 
+	/**
+	 * Starts a new bulk draw. Call this before calling any draw methods.
+	 */
 	public void begin() {
 		sb.begin();
 	}
 
+	/**
+	 * Ends the bulk draw. This sorts the given draw calls and executes the
+	 * original batch's draw functions in the order specified.
+	 */
 	public void end() {
 		drawCalls.sort((dca, dcb) -> dca.fst.compareTo(dcb.fst));
 		for (Pair<Float, Runnable> pair : drawCalls) {
@@ -66,6 +117,14 @@ public class BioVizSpriteBatch {
 		sb.end();
 	}
 
+	/**
+	 * Draws a message.
+	 * @param messageFont the font to use.
+	 * @param message the message to draw.
+	 * @param startX the x coordinate.
+	 * @param startY the y coordinate.
+	 * @param m the transformation matrix to use.
+	 */
 	public void drawMessage(
 			BitmapFont messageFont,
 			String message,
@@ -85,6 +144,14 @@ public class BioVizSpriteBatch {
 				}));
 	}
 
+	/**
+	 * Draws a message.
+	 * @param messageFont the font to use.
+	 * @param message the message to draw.
+	 * @param startX the x coordinate.
+	 * @param startY the y coordinate.
+	 * @param m the transformation matrix to use.
+	 */
 	public void drawMessage(
 			BitmapFont messageFont,
 			GlyphLayout message,
@@ -104,6 +171,10 @@ public class BioVizSpriteBatch {
 				}));
 	}
 
+	/**
+	 * Retrieves the current projection matrix.
+	 * @return the matrix that is currently in use.
+	 */
 	public Matrix4 getProjectionMatrix() {
 		return sb.getProjectionMatrix();
 	}
