35271879a1bb08f54f0f6f843528a9d6b9624f7a
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-05-23 16:28:40 +0200

    Checkstyle: FluidicConstraintViolation

--- core/src/de/bioviz/structures/FluidicConstraintViolation.java
+++ core/src/de/bioviz/structures/FluidicConstraintViolation.java
@@ -1,21 +1,58 @@
 package de.bioviz.structures;
 
 /**
- * Class that stores violations of fluidic constraints
+ * Class that stores violations of fluidic constraints.
  *
  * @author Oliver Keszocze
  */
 public class FluidicConstraintViolation {
 
 
+	/**
+	 * The first droplet that is involved in the violation.
+	 */
 	public final Droplet d1;
+
+	/**
+	 * The second droplet that is involved in the violation.
+	 */
 	public final Droplet d2;
+
+	/**
+	 * The field the first droplet is on when violating the constraints.
+	 */
 	public final BiochipField f1;
+
+	/**
+	 * The field the first droplet is on when violating the constraints.
+	 */
 	public final BiochipField f2;
+
+	/**
+	 * The time step the violation takes place.
+	 */
 	public final int timestep;
 
-	public FluidicConstraintViolation(Droplet d1, BiochipField f1, Droplet d2,
-									  BiochipField f2, int timestep) {
+	/**
+	 * Creates a constraint violation by directly setting all internal
+	 * variables.
+	 *
+	 * @param d1
+	 * 		The first droplet of the violation.
+	 * @param f1
+	 * 		The position of the first droplet.
+	 * @param d2
+	 * 		The second droplet of the violation.
+	 * @param f2
+	 * 		The position of the second droplet.
+	 * @param timestep
+	 * 		The timestep the violation occurs.
+	 */
+	public FluidicConstraintViolation(final Droplet d1,
+									  final BiochipField f1,
+									  final Droplet d2,
+									  final BiochipField f2,
+									  final int timestep) {
 		this.d1 = d1;
 		this.d2 = d2;
 		this.f1 = f1;
@@ -34,18 +71,29 @@ public class FluidicConstraintViolation {
 	 * @return true if the field is involved in this violation, false
 	 * otherwise.
 	 */
-	public boolean containsField(BiochipField field) {
+	public boolean containsField(final BiochipField field) {
 		return f1.equals(field) || f2.equals(field);
 	}
 
+	/**
+	 * @return String representation of the violation.
+	 */
 	public String toString() {
 		return "At timestep " + timestep + " droplet " + d1.getID() +
 			   " is too close to droplet " + d2.getID() + " : " + f1.pos +
-		" <-> " + f2.pos;
+			   " <-> " + f2.pos;
 	}
 
+	/**
+	 * Checks whether some object (ideally a violation) is equal to the given
+	 * violation.
+	 *
+	 * @param o
+	 * 		The other violation.
+	 * @return true if the violations are equal, false otherwise.
+	 */
 	@Override
-	public boolean equals(Object o) {
+	public boolean equals(final Object o) {
 		if (this == o) {
 			return true;
 		}
@@ -54,7 +102,7 @@ public class FluidicConstraintViolation {
 			return false;
 		}
 
-		FluidicConstraintViolation other = (FluidicConstraintViolation)o;
+		FluidicConstraintViolation other = (FluidicConstraintViolation) o;
 
 		boolean direct1 = f1.equals(other.f1) && d1.equals(other.d1);
 		boolean direct2 = f2.equals(other.f2) && d2.equals(other.d2);
@@ -65,7 +113,8 @@ public class FluidicConstraintViolation {
 		boolean switched = switched1 && switched2;
 
 		System.out.println("direct1: " + direct1 + " direct2 " + direct2);
-		System.out.println("switched1: " + switched1 + " switched " + switched2);
+		System.out.println(
+				"switched1: " + switched1 + " switched " + switched2);
 
 		return (timestep == other.timestep) && (direct || switched);
 
@@ -76,6 +125,6 @@ public class FluidicConstraintViolation {
 	 * Pretty simple and bad hashCode implementation.
 	 */
 	public int hashCode() {
-		return timestep+f1.x()+f1.y()+f2.x()+f2.y();
+		return timestep + f1.x() + f1.y() + f2.x() + f2.y();
 	}
 }
