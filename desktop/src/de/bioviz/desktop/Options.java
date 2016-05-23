1223149949e51de3cc2cdd74f3f04350e8c4f8b7
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-05-23 15:16:52 +0200

    Re-format Options class

--- desktop/src/de/bioviz/desktop/Options.java
+++ desktop/src/de/bioviz/desktop/Options.java
@@ -1,11 +1,8 @@
 package de.bioviz.desktop;
 
-import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Contains all options that can be accessed via the command line.
@@ -14,17 +11,19 @@ import java.util.List;
  */
 public class Options {
 
-	@Option(name="-c",aliases="--check",usage="check .bio file for errors",metaVar="file")
+	@Option(name = "-c", aliases = "--check",
+			usage = "check .bio file for errors",
+			metaVar = "file")
 	public File check;
 
-	@Option(name="-h",aliases="--help", usage="print help message")
-	public boolean help=false;
+	@Option(name = "-h", aliases = "--help", usage = "print help message")
+	public boolean help = false;
 
-	@Option(name="-a",aliases="--authors",usage="print authors")
-	public boolean authors=false;
+	@Option(name = "-a", aliases = "--authors", usage = "print authors")
+	public boolean authors = false;
 
-	@Option(name="--version",usage="print version")
-	public boolean version=false;
+	@Option(name = "--version", usage = "print version")
+	public boolean version = false;
 
 
 }
