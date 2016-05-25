6b9a685f39cff89677ee520ab4af52f8e8e44acc
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-05-25 16:03:38 +0200

    Make BioVizInfo SonarQube compliant

--- core/src/de/bioviz/util/BioVizInfo.java
+++ core/src/de/bioviz/util/BioVizInfo.java
@@ -5,8 +5,22 @@ package de.bioviz.util;
  * @author Oliver Keszocze
  */
 public class BioVizInfo {
-	public static final String version = "0.8";
-	public static final String[] authors = {
+
+
+
+
+	public static final String VERSION = "0.8";
+	private static final String[] authors = {
 			"Oliver Keszocze", "Jannis Stoppe", "Maximilian Luehnert"
 	};
+
+	public static String[] authors() {
+		return authors.clone();
+	}
+
+	/**
+	 * Prevent instantiation of this class
+	 */
+	private BioVizInfo() {}
+
 }
