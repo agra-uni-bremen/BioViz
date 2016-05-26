b582a8e713dc076fdeb471da1a80e09f2f61ba74
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-05-26 10:57:23 +0200

    Make MsgAppender more checkstyle conform

--- core/src/de/bioviz/messages/MsgAppender.java
+++ core/src/de/bioviz/messages/MsgAppender.java
@@ -12,48 +12,56 @@ import de.bioviz.ui.BioViz;
  * @author Oliver Keszocze
  */
 public class MsgAppender extends AppenderBase<ILoggingEvent> {
-	/**
-	 * The "parent" MessageCenter.
-	 */
-	MessageCenter mc = null;
+
 
 	/**
 	 * The "parent" visualization.
 	 */
 	private static BioViz viz;
 
-    @Override
-    /**
-     * Forwards a log message to the MessageCenter that will display it on the HUD.
-     */
-    protected void append(ILoggingEvent eventObject) {
-        if (isStarted() && eventObject.getLevel().isGreaterOrEqual(Level.INFO)) {
-            mc.addMessage(eventObject.getFormattedMessage());
-        }
-    }
 
-    @Override
-    /**
-     * Checks whether this appender is ready to append messages.
-     *
-     * The idea is to check whether a GUI is present. If so, messages can be appended.
-     */
-    public boolean isStarted() {
-        if (mc == null) {
-            if (viz != null && viz.messageCenter != null) {
-                mc = viz.messageCenter;
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    }
-    
 	/**
-	 * Sets the reference to the {@link BioViz} that is supposed to show the
-	 *+ messages.
+	 * The "parent" MessageCenter.
+	 */
+	MessageCenter mc = null;
+
+	@Override
+	/**
+	 * Forwards a log message to the MessageCenter that will display it on the
+	 * HUD.
+	 */
+	protected void append(final ILoggingEvent eventObject) {
+		if (isStarted() &&
+			eventObject.getLevel().isGreaterOrEqual(Level.INFO)) {
+			mc.addMessage(eventObject.getFormattedMessage());
+		}
+	}
+
+	@Override
+	/**
+	 * Checks whether this appender is ready to append messages.
+	 *
+	 * The idea is to check whether a GUI is present. If so, messages can be
+	 * appended.
+	 */
+	public boolean isStarted() {
+		if (mc == null) {
+			if (viz != null && viz.messageCenter != null) {
+				mc = viz.messageCenter;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return true;
+		}
+	}
+
+	/**
+	 * Sets the reference to the {@link BioViz} that is supposed to show the +
+	 * messages.
 	 *
 	 * @param bioViz
 	 * 		The {@link BioViz} that is supposed to show the messages.
