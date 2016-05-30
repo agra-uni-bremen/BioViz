53ffda79ebac12166f6f04249a2cb11b66ab8a7f
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-05-31 00:11:26 +0200

    Checkstylify DrawableLine

--- core/src/de/bioviz/ui/DrawableLine.java
+++ core/src/de/bioviz/ui/DrawableLine.java
@@ -5,24 +5,25 @@ import com.badlogic.gdx.math.Vector2;
 
 public class DrawableLine extends DrawableSprite {
 
-	public Vector2 from, to;
+    public Vector2 from;
+    public Vector2 to;
 
-	public DrawableLine(BioViz parent) {
-		super(TextureE.BlackPixel, parent);
-		this.setZ(DisplayValues.DEFAULT_LINE_DEPTH);
-	}
+    public DrawableLine(final BioViz parent) {
+        super(TextureE.BlackPixel, parent);
+        this.setZ(DisplayValues.DEFAULT_LINE_DEPTH);
+    }
 
-	public void draw() {
-		Color col = this.getColor();
-		Vector2 toTarget = from.cpy().sub(to);
-		final float len = toTarget.len(); 
-		setX(viz.currentCircuit.xCoordOnScreen((to.x + from.x) / 2f));
-		setY(viz.currentCircuit.yCoordOnScreen((to.y + from.y) / 2f));
-		setScaleX(viz.currentCircuit.getSmoothScale() * len);
-		setScaleY(2f);
-		setRotation((float) (Math.atan2(toTarget.y, toTarget.x) * 
-				(180f / Math.PI)));
-		setColorImmediately(col);
-		super.draw();
-	}
+    public void draw() {
+        Color col = this.getColor();
+        Vector2 toTarget = from.cpy().sub(to);
+        final float len = toTarget.len();
+        setX(viz.currentCircuit.xCoordOnScreen((to.x + from.x) / 2f));
+        setY(viz.currentCircuit.yCoordOnScreen((to.y + from.y) / 2f));
+        setScaleX(viz.currentCircuit.getSmoothScale() * len);
+        setScaleY(2f);
+        setRotation((float) (Math.atan2(toTarget.y, toTarget.x) *
+                (180f / Math.PI)));
+        setColorImmediately(col);
+        super.draw();
+    }
 }
