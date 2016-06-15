06de792bb4dc4d93c55cbd3fadb46553d4c2fc14
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2016-06-15 17:47:20 +0200

    Change a lot of access modifiers in the messages package
    The new ones are way more restrictive.

--- core/src/de/bioviz/messages/HUDMessage.java
+++ core/src/de/bioviz/messages/HUDMessage.java
@@ -26,13 +26,13 @@ public class HUDMessage extends MessageBase {
 	 * Creates a HUDMessage that is to be displayed at the specified position.
 	 *
 	 * The initial size of the message is negative.
-	 * @TODO What does this actually mean?
+	 * TODO What does this actually mean?
 	 *
 	 * @param message The message to display
 	 * @param x The x-position for the message
 	 * @param y The y-position for the message
 	 */
-	public HUDMessage(final String message, final float x, final float y) {
+	HUDMessage(final String message, final float x, final float y) {
 		super(message);
 		this.x = x;
 		this.y = y;
