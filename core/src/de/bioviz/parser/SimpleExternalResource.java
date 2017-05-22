8da3168c6d8ae1d28857527cb4b8074b5f92eae5
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2017-05-22 17:22:15 +0200

    Add simple class for storing parsing data
    This class stores information on sinks/dispensers. It is only
    used to make the code more readable and has little semantic meaning.
    
    This class is only necessary as Java does not support type aliases.

--- core/src/de/bioviz/parser/SimpleExternalResource.java
+++ core/src/de/bioviz/parser/SimpleExternalResource.java
@@ -1,7 +1,48 @@
 package de.bioviz.parser;
 
+import de.bioviz.structures.Direction;
+import de.bioviz.structures.Point;
+
+import java.util.Optional;
+
 /**
- * Created by keszocze on 22.05.17.
+ * Convenience class to store commonly used data for external resources.
+ *
+ * The idea is to store the position on the grid and the direction, from which
+ * droplets are dispensed/to which droplets are removed. This class is intended
+ * to be used within the parsing process only.
+ *
+ * This class is only necessary as Java does not have type aliases.
+ * There is no real inherent semantics to this class
  */
 public class SimpleExternalResource {
+
+		final Point gridPosition;
+		final Direction dropletDirection;
+		final Optional<Integer> id;
+
+
+	/**
+	 * @brief Constructor for creating a sink
+	 * @param pos Position on the grid
+	 * @param dir The direction in which the droplet is removed
+	 */
+		public SimpleExternalResource(Point pos, Direction dir) {
+			gridPosition = pos;
+			dropletDirection = dir;
+			id = Optional.empty();
+		}
+
+	/**
+	 * @brief Cosntructor for creating a dispenser
+	 * @param pos Position on the grid
+	 * @param dir The direction from which the droplet is dispensed
+	 * @param id The type of fluid that is dispensed
+	 */
+		public SimpleExternalResource(Point pos, Direction dir, Integer id) {
+			gridPosition = pos;
+			dropletDirection = dir;
+			this.id = Optional.of(id);
+		}
+
 }
