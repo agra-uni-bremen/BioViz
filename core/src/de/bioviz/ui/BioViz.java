5defe26992e16dcfeab841ba42ea5459338efcca
Oliver Keszöcze <keszocze@informatik.uni-bremen.de>
2017-05-30 17:32:14 +0200

    Remove unused import

--- core/src/de/bioviz/ui/BioViz.java
+++ core/src/de/bioviz/ui/BioViz.java
@@ -22,7 +22,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.Vector;
+
 
 
 public class BioViz implements ApplicationListener {
