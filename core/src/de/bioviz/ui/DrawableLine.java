95091b2e71641fe331316288afec454acc288e8e
Maximilian Luenert <malu@informatik.uni-bremen.de>
2017-06-19 15:37:50 +0200

    Add arrowHead to drawable line.

--- core/src/de/bioviz/ui/DrawableLine.java
+++ core/src/de/bioviz/ui/DrawableLine.java
@@ -3,6 +3,8 @@ package de.bioviz.ui;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 
+import static de.bioviz.ui.TextureE.ArrowHead;
+
 /**
  * A simple line segment defined by the start and end point.
  *
@@ -22,6 +24,8 @@ public class DrawableLine extends DrawableSprite {
 	 */
     public Vector2 to;
 
+    private DrawableArrowHead head;
+
 	/**
 	 * Creates a drawable line with no start/end points.
 	 * @param parent The parent BioViz instances.
@@ -29,6 +33,7 @@ public class DrawableLine extends DrawableSprite {
     public DrawableLine(final BioViz parent) {
         super(TextureE.BlackPixel, parent);
         this.setZ(DisplayValues.DEFAULT_LINE_DEPTH);
+        head = new DrawableArrowHead(parent);
     }
 
 	/**
@@ -41,10 +46,19 @@ public class DrawableLine extends DrawableSprite {
         setX(viz.currentAssay.xCoordOnScreen((to.x + from.x) / 2f));
         setY(viz.currentAssay.yCoordOnScreen((to.y + from.y) / 2f));
         setScaleX(viz.currentAssay.getSmoothScale() * len);
-        setScaleY(2f);
-        setRotation((float) (Math.atan2(toTarget.y, toTarget.x) *
-                (180f / Math.PI)));
+        setScaleY(3f);
+				float rotation = (float) (Math.atan2(toTarget.y, toTarget.x) *
+					(180f / Math.PI));
+        setRotation(rotation);
         setColorImmediately(col);
+
+				head.setColorImmediately(col);
+				head.setX(viz.currentAssay.xCoordOnScreen(to.x));
+				head.setY(viz.currentAssay.yCoordOnScreen(to.y));
+				head.setScaleX(viz.currentAssay.getSmoothScale());
+				head.setScaleY(viz.currentAssay.getSmoothScale() * 0.5f);
+				head.setRotation(rotation+180f);
+				head.draw();
         super.draw();
     }
 }
