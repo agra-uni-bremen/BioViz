0ba8b682cba1ea537722e3436bb01c1bb1e5fba4
Maximilian Luenert <malu@informatik.uni-bremen.de>
2017-06-27 11:13:17 +0200

    Update imports.

--- core/src/de/bioviz/svg/SVGManager.java
+++ core/src/de/bioviz/svg/SVGManager.java
@@ -24,12 +24,20 @@ import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
